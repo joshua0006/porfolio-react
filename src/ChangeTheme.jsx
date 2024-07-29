c798e20fa66a1c208bf6c08f604a5daf61acddde
joshua0006 <magnasejoshua@gmail.com>
2024-07-29 13:30:37 +0800

    done

--- src/ChangeTheme.jsx
+++ src/ChangeTheme.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 const ChangeTheme = ({setTheme , setFont}) => {
-    const fontStyle = {fontFamily: 'Cursive'};
+    const fontStyle = {
+        fontFamily: 'Kanit'
+    }
+    
     return ( 
         
         <div className="change-theme">
-            <div className="color" style={fontStyle}>
-                    <p>Change Color:</p>
+            <div className="color" >
+                    <p style={fontStyle}>Change Color:</p>
                     <div>
                         <button onClick={() => setTheme("tomato")} className="tomato"></button>
                         <button onClick={() => setTheme("orange")} className="orange"></button>
@@ -12,13 +17,21 @@ const ChangeTheme = ({setTheme , setFont}) => {
                         <button onClick={() => setTheme("teal")} className="teal-blue"></button>
                     </div>
                 </div> 
-                <div className="font" style={fontStyle}>
-                    <p>Change Font:</p>
+                <div className="font">
+                    <p style={fontStyle}>Change Font:</p>
                     <div>
-                        <button onClick={() => setFont("Arial")}>1</button>
-                        <button onClick={() => setFont("Serif")}>2</button>
-                        <button onClick={() => setFont("Times")}>3</button>
-                        <button onClick={() => setFont("Cursive")}>4</button>
+                        <button className="font-color" 
+                                onClick={() => setFont("Kanit")} 
+                                style={fontStyle}>1</button>
+                        <button className="font-color" 
+                                onClick={() => setFont("Serif")} 
+                                style={fontStyle}>2</button>
+                        <button className="font-color" 
+                                onClick={() => setFont("Times")} 
+                                style={fontStyle}>3</button>
+                        <button className="font-color" 
+                                onClick={() => setFont("Cursive")} 
+                                style={fontStyle}>4</button>
                     </div>
                 </div> 
         </div>
